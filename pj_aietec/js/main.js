0eae74dca1119764f9f8531fadde332e398bfa8b
ukowork <ukowork@gmail.com>
2021-05-26 09:12:53 +0800

    重新分配專案路徑

---
+++ pj_aietec/js/main.js
@@ -0,0 +1,123 @@
+jQuery(document).ready(function($){
+	//set your google maps parameters
+	var latitude = 22.579295,
+		longitude = 120.424958,
+		map_zoom = 15;
+
+	//google map custom marker icon - .png fallback for IE11
+	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+	var marker_url = ( is_internetExplorer11 ) ? 'images/map.png' : 'images/cd-icon-location.svg';
+		
+	//define the basic color of your map, plus a value for saturation and brightness
+	var	main_color = '#2d313f',
+		saturation_value= -20,
+		brightness_value= 5;
+
+	//we define here the style of the map
+	var style= [{"featureType":"all","stylers":[{"saturation":0},{"hue":"#e7ecf0"}]},{"featureType":"road","stylers":[{"saturation":-70}]},{"featureType":"transit","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"visibility":"simplified"},{"saturation":-60}]}];
+		
+	//set google map options
+	var map_options = {
+      	center: new google.maps.LatLng(latitude, longitude),
+      	zoom: map_zoom,
+      	panControl: false,
+      	zoomControl: false,
+      	mapTypeControl: false,
+      	streetViewControl: false,
+      	mapTypeId: google.maps.MapTypeId.ROADMAP,
+      	scrollwheel: false,
+      	styles: style,
+    }
+    //inizialize the map
+	var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+	//add a custom marker to the map				
+	var marker = new google.maps.Marker({
+	  	position: new google.maps.LatLng(latitude, longitude),
+	    map: map,
+	    visible: true,
+	 	icon: marker_url,
+	});
+
+	//add custom buttons for the zoom-in/zoom-out on the map
+	function CustomZoomControl(controlDiv, map) {
+		//grap the zoom elements from the DOM and insert them in the map 
+	  	var controlUIzoomIn= document.getElementById('cd-zoom-in'),
+	  		controlUIzoomOut= document.getElementById('cd-zoom-out');
+	  	controlDiv.appendChild(controlUIzoomIn);
+	  	controlDiv.appendChild(controlUIzoomOut);
+
+		// Setup the click event listeners and zoom-in or out according to the clicked element
+		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+		    map.setZoom(map.getZoom()+1)
+		});
+		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+		    map.setZoom(map.getZoom()-1)
+		});
+	}
+
+	var zoomControlDiv = document.createElement('div');
+ 	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+
+  	//insert the zoom div on the top left of the map
+  	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+	
+	
+	 var contentString = '<div class="mapt">總公司</div>'+'高雄市大寮區(大發工業區)莒光二街20號<br>'+
+	 'TEL : 886-7-7877690／FAX : 886-7-7870942';
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  
+  //CENTER
+     google.maps.event.addDomListener(window, "resize", function() {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
+	
+});
+
+
+/////////////////////////////////////////////////////////////////
+
+
+$(document).ready(function() {
+
+  var toggleAffix = function(affixElement, scrollElement, wrapper) {
+  
+    var height = affixElement.outerHeight(),
+        top = wrapper.offset().top;
+    
+    if (scrollElement.scrollTop() >= 400){
+        wrapper.height(height);
+        affixElement.addClass("affix");
+		affixElement.removeClass("affix-top");
+    }
+    else {
+		affixElement.addClass("affix-top");
+        affixElement.removeClass("affix");
+        wrapper.height('auto');
+    }
+      
+  };
+  
+
+  $('[data-toggle="affix"]').each(function() {
+    var ele = $(this),
+        wrapper = $('<div></div>');
+    
+    ele.before(wrapper);
+    $(window).on('scroll resize', function() {
+        toggleAffix(ele, $(this), wrapper);
+    });
+    
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
+  
+});
---
+++ pj_ible/js/main.js
@@ -0,0 +1,82 @@
+$(document).ready(function () {
+    document.querySelector('#scroll-down').addEventListener("click", scrollDown);
+    document.querySelector('#scroll-top').addEventListener("click", scrollUP);
+    //
+
+    $(window).on('scroll', function () {
+        //
+        let fastElement = $('.scroll_fast');
+        //let fastElement = document.querySelectorAll('.scroll_fast');
+        //
+        // https://developer.mozilla.org/zh-TW/docs/Web/API/window/requestAnimationFrame
+        window.requestAnimationFrame(function () {
+            // 綁定在有 class 的每個元素上
+            for (let i = 0; i < fastElement.length; i++) {
+                let currentElement = fastElement.eq(i);
+                //
+                let windowTop = $(window).scrollTop(), // 瀏覽器網頁最高點 到 網頁真實最高點 的距離 = 滾動量
+                    elementTop = currentElement.offset().top, // 元素到瀏覽器最高點的距離
+                    elementHeight = currentElement.height(), // 元素本體高度
+                    // 可視高 = 瀏覽器網頁高 * 0.5 減 元素本體高 * 0.5
+                    viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+                    scrolled = windowTop - elementTop + viewPortHeight; // 偏移量 = 滾動量 減掉 元素到瀏覽器最高點的距離 加上 可視高
+                // 還不太明白為什麼要這樣算
+                // 動態計算 transform 3d 的 y 位移 並指定給 dom
+                currentElement.css({
+                    transform: 'translate3d(0,' + scrolled * -0.5 + 'px, 0)'
+                });
+            }
+        });
+        //
+        // ----------------------------------------
+        // 滾到相應處才撥放動畫
+
+        let windowTop = $(window).scrollTop();
+        console.log('windowTop: ' + windowTop);
+        //
+        let el = ['.prd-title-block .block-title',
+                  '.prd-content-block .prd-img img',
+                  '.prd-content-block .prd-text h1',
+                  '.prd-content-block .prd-text p:nth-child(2)',
+                  '.prd-content-block .prd-text p:nth-child(3)',
+                  '.prd-content-block button'];
+        //
+        if (windowTop >= 800) {
+            for (let i = 0; i < el.length; i++) {
+                document.querySelector('#prd_1 ' + el[i]).classList.remove('none-opacity');
+                document.querySelector('#prd_1 ' + el[i]).classList.add('fade-up-top');
+            }
+        } else if (windowTop < 800) {
+            for (let i = 0; i < el.length; i++) {
+                document.querySelector('#prd_1 ' + el[i]).classList.add('none-opacity');
+                document.querySelector('#prd_2 ' + el[i]).classList.add('none-opacity');
+            }
+        }
+        //
+        if (windowTop >= 1300) {
+            for (let i = 0; i < el.length; i++) {
+                document.querySelector('#prd_2 ' + el[i]).classList.remove('none-opacity');
+                document.querySelector('#prd_2 ' + el[i]).classList.add('fade-up-top');
+            }
+        }
+    });
+});
+
+
+function scrollDown(e) {
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    const offsetTop = document.querySelector(href).offsetTop;
+    //
+    window.scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+}
+
+function scrollUP() {
+    window.scroll({
+        top: 0,
+        behavior: "smooth"
+    });
+}
